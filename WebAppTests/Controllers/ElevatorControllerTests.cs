c7bdb3ad6bbee0937a54072b4733a9beb24a7100
William Forney <bill@improvgroup.com>
2021-05-23 18:38:45 -0700

    Add unit test project

---
+++ WebAppTests/Controllers/ElevatorControllerTests.cs
@@ -0,0 +1,34 @@
+namespace WebApp.Controllers.Tests
+{
+	using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+	using WebApp.Controllers;
+
+	[TestClass()]
+	public class ElevatorControllerTests
+	{
+		[TestMethod()]
+		public void ElevatorControllerTest()
+		{
+			Assert.Fail();
+		}
+
+		[TestMethod()]
+		public void CallElevatorToTest()
+		{
+			Assert.Fail();
+		}
+
+		[TestMethod()]
+		public void GetNextFloorTest()
+		{
+			Assert.Fail();
+		}
+
+		[TestMethod()]
+		public void GetRequestedFloorsTest()
+		{
+			Assert.Fail();
+		}
+	}
+}
